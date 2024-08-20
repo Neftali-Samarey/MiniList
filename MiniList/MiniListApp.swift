5d7946b4fef3ad5edd0496b4481c3518f7b351de
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2024-08-20 15:54:38 -0400

    Initial Commit

---
+++ MiniList/MiniListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniListApp.swift
+//  MiniList
+//
+//  Created by Neftali Samarey on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
